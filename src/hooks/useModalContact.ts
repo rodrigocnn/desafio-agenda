b3b11e2ebd46247e7d0a735cb14bb4d228a5952e
Rodrigo César <rodrigocnn@gmail.com>
2024-03-23 14:16:59 -0300

    feat(component):  create useModalContacts

---
+++ src/hooks/useModalContact.ts
@@ -0,0 +1,40 @@
+import { useContext, useState } from "react";
+import { toast } from "react-toastify";
+
+import { ContactsContext } from "../shared/context/ContactsContext";
+import { Contact } from "../shared/interfaces";
+import { api } from "../services/api";
+
+export function useModalContact() {
+  const { openModal, setOpenModal } = useContext(ContactsContext);
+  const [contact, setContact] = useState<Contact>();
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const fieldName = event.target.name;
+    const value = event.target.value;
+    setContact({ ...contact, [fieldName]: value } as Contact);
+  };
+
+  const onSubmit = async (
+    event: React.FormEvent<EventTarget | HTMLFormElement>
+  ) => {
+    event.preventDefault();
+    const response = await api.post("contacts", contact);
+    if (response.data) {
+      toast("Novo Contato Salvo com Sucesso", { type: "success" });
+    }
+  };
+
+  return {
+    openModal,
+    handleCloseModal,
+    handleChange,
+    onSubmit,
+  };
+}
