b247ea52b8039bc5725df3063e0334f3a7dd210a
Rodrigo César <rodrigocnn@gmail.com>
2024-03-23 11:04:31 -0300

    feat(hook) create useTableContacts

---
+++ src/hooks/useTableContacts.ts
@@ -0,0 +1,30 @@
+import { useContext, useEffect } from "react";
+import { ContactsContext } from "../shared/context/ContactsContext";
+import { api } from "../services/api";
+
+export function useTableContacts() {
+  const { contacts, setContacts } = useContext(ContactsContext);
+
+  async function getContacts() {
+    try {
+      const response = await api.get("contacts");
+      return response.data;
+    } catch (error) {
+      console.error("Erro ao obter contatos:", error);
+      return [];
+    }
+  }
+
+  useEffect(() => {
+    async function fetchContacts() {
+      const fetchedContacts = await getContacts();
+      setContacts(fetchedContacts);
+    }
+
+    fetchContacts();
+  }, []);
+
+  return {
+    contacts,
+  };
+}
