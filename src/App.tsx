be8756c996a547ffda00da1516ee5798235354e2
Rodrigo César <rodrigocnn@gmail.com>
2024-03-22 22:31:49 -0300

    chore(component): config file app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { GlobalStyle } from "./styles/global";
+import { ContactsProvider } from "./shared/context/ContactsContext";
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { ModalContact } from "./components/ModalContact";
+
 function App() {
-  return <h1>Vite + React</h1>;
+  return (
+    <ContactsProvider>
+      <Header />
+      <GlobalStyle />
+      <Dashboard />
+      <ModalContact />
+    </ContactsProvider>
+  );
 }
 
 export default App;
