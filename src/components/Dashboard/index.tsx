a6571f99fe9d95a3b2e8ec02b24d80b28a0b6ae5
Rodrigo César <rodrigocnn@gmail.com>
2024-03-22 22:28:41 -0300

    feat(component): create component dashboard

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import { TableContacts } from "../TableContacts";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <TableContacts />
+    </Container>
+  );
+}
