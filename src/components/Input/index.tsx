414192eaf51ab18430f4250843cd7f848f2abbca
Rodrigo César <rodrigocnn@gmail.com>
2024-03-24 08:19:27 -0300

    feat(component) create input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+import { InputStyled } from "./styles";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value?: string;
+  error?: boolean;
+}
+
+export const Input: React.FC<InputProps> = ({ error, value, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  console.log("error", error);
+
+  useEffect(() => {
+    if (error && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [error]);
+
+  return <InputStyled ref={inputRef} error={error} {...rest} value={value} />;
+};
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+interface InputProps {
+  error?: boolean;
+  ref: React.RefObject<HTMLInputElement>;
+}
+
+export const InputStyled = styled.input<InputProps>`
+  width: 100%;
+  padding: 0 1.5rem;
+  height: 2.5rem;
+  border-radius: 0.25rem;
+  border: 1px solid #d7d7d7;
+  background: #e7e9ee;
+  font-weight: 400;
+  font-size: 1rem;
+  &::placeholder {
+    color: var(--text-body);
+  }
+
+  &:focus {
+    border: 1px solid ${(props) => (props.error ? "red !important" : "blue")};
+    outline: none;
+  }
+`;
