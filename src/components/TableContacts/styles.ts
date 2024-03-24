9f0de5b43a9f26b9f542ea9714360005a115c67d
Rodrigo César <rodrigocnn@gmail.com>
2024-03-24 10:07:29 -0300

    fix(style) removed brackets

--- src/components/TableContacts/styles.ts
+++ src/components/TableContacts/styles.ts
@@ -7,33 +7,31 @@ export const Container = styled.div`
     width: 100%;
     border-spacing: 0 0.5rem;
 
-    thead{
+    thead {
       tr {
-          background: var(--blue);
+        background: var(--blue);
 
-          th {
-            color: var(--shape);
-          }
+        th {
+          color: var(--shape);
         }
       }
     }
+  }
 
+  th {
+    color: var(--text-body);
+    font-weight: 400;
+    padding: 1rem 2rem;
+    text-align: left;
+    line-height: 1.5rem;
+  }
 
-    th {
-      color: var(--text-body);
-      font-weight: 400;
-      padding: 1rem 2rem;
-      text-align: left;
-      line-height: 1.5rem;
-    }
-
-    td {
-      padding: 1rem 2rem;
-      border: 0;
-      background: var(--shape);
-      color: var(--text-body);
-      border-radius: 0.25rem;
-    }
+  td {
+    padding: 1rem 2rem;
+    border: 0;
+    background: var(--shape);
+    color: var(--text-body);
+    border-radius: 0.25rem;
   }
 `;
 
